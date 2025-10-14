0b679064560ab0bc1fd32876fda4771bc6a21bdc
Prathamesh <prathameshdhadankar07@gmail.com>
2025-10-15 00:47:08 +0530

    added code to trees

--- Trees/prog4.java
+++ Trees/prog4.java
@@ -1,29 +1,36 @@
 package Trees;
 
-// depth is of a node and maxDepth is of whole tree
+/*
+    The diameter of a tree is the longest path (in edges) between any two nodes in the tree.
+    This path may or may not pass through the root.
+*/
 
-class DepthOfTree {
-    
-    int depth(TreeNode root, int target){
-        if(root == null) return -1;
-
-        if(root.data == target) return 0; // root depth = 0
-
-        int left = depth(root.left, target);
-        if(left != -1) return left+1;
-
-        int right = depth(root.right, target);
-        if(right != -1) return right+1;
-
-        return -1;
+class Pair {
+    int height;
+    int diameter;
+    Pair(int h, int d){
+        height = h;
+        diameter = d;
     }
+}
 
-    int maxDepth(TreeNode root){
-        if(root == null) return 0;
+class DiameterOfTree {
 
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
+    Pair diameterHelper(TreeNode root){
+        if(root == null) return new Pair(-1, 0);
 
-        return 1 + Math.max(left, right);
+        Pair left = diameterHelper(root.left);
+        Pair right = diameterHelper(root.right);
+
+        int height = 1 + Math.max(left.height, right.height);
+        int pathThroughRoot = left.height + right.height + 2;
+
+        int diameter = Math.max(pathThroughRoot, Math.max(left.diameter, right.diameter));
+
+        return new Pair(height, diameter);
+    }
+    
+    int diameter(TreeNode root){
+        return diameterHelper(root).diameter;
     }
 }
---
+++ Trees/prog6.java
@@ -0,0 +1,15 @@
+package Trees;
+
+class LCAOfBT {
+    
+    TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q){
+        if(root == null || root == p || root == q) return root;
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if(left == null) return right;
+        else if(right == null) return left;
+        else return root; // if both are not null
+    }
+}
